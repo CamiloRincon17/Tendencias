494f12ffab6e3d7476b9f4804d31831366e5f9d7
camilo <camilo171512@gmail.com>
2026-03-18 11:15:42 -0500

    Actualizar proyecto Tendencias

--- proyecto/routes/web.php
+++ proyecto/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('inicio');
+Route::get('/suma', function () {
+    return view('suma');
 });
+Route::post('/suma', function (Request $request) {
+    $num1=$request->input('num1');
+    $num2=$request->input('num2');
+    $suma=$num1+$num2;
+    return view('suma', compact('suma'));
+    echo "La suma de $num1 y $num2 es $suma";
+});
+
+
+ 
